a6d27a4d48813e6c0e38c0c5ce798200bf9dfa28
Aleu79 <aleeaguero79@gmail.com>
2024-10-04 01:49:48 -0300

    comments scrips

--- importdate.py
+++ importdate.py
@@ -6,11 +6,17 @@ import django
 import sys
 
 sys.path.append('/home/aguero/djngo-exer')
+#aca es la ruta de la carpeta "djngo-exer" que es la misma que te pwd osea
+#queria tu/ruta/djngo-exer
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Proyecto.settings')
 django.setup()
 
 from App.models import Producto
 ruta = '/home/aguero/djngo-exer/productos_ferreteria.csv'
+#acaes la ruta donde el archivo .csv que en este caso es "productos_ferreteria.csv" 
+#asi que anda a donde esta la carpeta "djngo-exer" y en la consola pone "pwd" y luego
+#te va a dar una ruta de donde esta esa carpeta osea "djngo-exer" y ahi pones el .csv
+#entonces quedaria tu/ruta/djngo-exer/productos_ferreteria.csv
 
 if os.path.exists(ruta):
     with open(ruta, newline='', encoding='utf-8') as csvfile:
