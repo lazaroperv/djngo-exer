c69dec74f9fcd3654a23322a23577f98a596ca82
Aleu79 <aleeaguero79@gmail.com>
2024-10-04 16:01:16 -0300

    commit 6

--- App/views.py
+++ App/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .forms import *  # Asegúrate de que tienes los formularios correctamente definidos
-from .models import Producto, Compra  # Importación de tus modelos
-from django.contrib.auth import logout  # Importamos la Librería de Logout
+from .forms import *  
+from .models import Producto, Compra 
+from django.contrib.auth import logout 
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib import messages  # Para mostrar mensajes de error o éxito
 
 # Create your views here.
 
-# Página principal de la ferretería, mostrando los últimos 10 productos añadidos
 def Home(request):
     buscar = Producto.objects.all().order_by('-codigo')[:10]  # Traemos los últimos 10 productos
     data = {
@@ -44,23 +43,21 @@ def comprar_producto(request, producto_id):
 
     # Simulamos la compra verificando el stock disponible
     if producto.stock > 0:
-        producto.stock -= 1  # Reducir el stock en 1
-        producto.save()  # Guardar cambios en el producto
+        producto.stock -= 1  
+        producto.save()  
 
         # Crear la compra
         compra = Compra.objects.create(
             usuario=request.user,  # Usuario autenticado
-            producto=producto,     # Producto comprado
-            cantidad=1,            # Por ahora, 1 unidad
+            producto=producto,    
+            cantidad=1,          
             precio_total=producto.precio  # El precio total es igual al precio del producto
         )
         compra.save()  # Guardar la compra en la base de datos
 
-        # Mostrar un mensaje de éxito y redirigir a una página de confirmación
         messages.success(request, f"Has comprado {producto.nombre} con éxito.")
         return render(request, 'Pages/confirmacion_compra.html', {'producto': producto})
 
-    # Si no hay stock disponible, mostramos un mensaje de error
     else:
         messages.error(request, f"Lo siento, no hay stock disponible para {producto.nombre}.")
         return render(request, 'Pages/sin_stock.html', {'producto': producto})
